33c6d2fe770ac709dc3967d13bb4e8f21285fa0d
ShiraSchw <shirgail1@gmail.com>
2025-05-28 22:52:50 +0300

    main work

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ app.secret_key = 'your-secret-key'
 app.register_blueprint(app_routes)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
