b502ffdbd9284e331c797334bc804bbef265c1e8
ShiraSchw <shirgail1@gmail.com>
2025-05-28 17:54:36 +0300

    fix port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from routes import app_routes
 
@@ -5,4 +7,5 @@ app = Flask(__name__)
 app.register_blueprint(app_routes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
