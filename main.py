51e8b36a70cbc205203fa067fda069157bae9986
ShiraSchw <shirgail1@gmail.com>
2025-05-29 12:09:14 +0300

    redirect('/app/')

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect
+
 from routes import app_routes
 
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'
 
 app.register_blueprint(app_routes, url_prefix='/app')
+
+@app.route('/')
+def redirect_to_app():
+    return redirect('/app/')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
