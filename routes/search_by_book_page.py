9c24d91fd39af418a8a46c621f4cbe81a84fd523
ShiraSchw <shirgail1@gmail.com>
2025-06-01 15:25:12 +0300

    subsub

--- routes/search_by_book_page.py
+++ routes/search_by_book_page.py
@@ -27,7 +27,7 @@ def search_by_book_home():
             category_tree[cat].setdefault(sub, [])
 
             if subsub:
-                category_tree[cat][sub].append(subsub)
+                category_tree[cat][sub].setdefault(subsub, [])
 
     return render_template("search_by_book_home.html", category_tree=category_tree)
 
